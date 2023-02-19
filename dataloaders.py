4d9451398c4fee400e465dce2cb595e3062b66e0
hustine316 <austinefrank14@gmail.com>
2023-02-19 10:55:49 +0100

    add dataloader

--- dataloaders.py
+++ dataloaders.py
@@ -79,10 +79,10 @@ def get_data(config):
     elif config.data.dataset == 'xray':
 
         transforms = torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
             torchvision.transforms.Grayscale(1),
             torchvision.transforms.Resize(
                 (config.data.image_size, config.data.image_size)),
+            torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize((0.5, ), (0.5, ))
         ])
         train_dataset = torchvision.datasets.ImageFolder(
